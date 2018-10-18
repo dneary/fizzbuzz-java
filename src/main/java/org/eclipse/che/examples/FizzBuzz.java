502fd3276dc1a8336de7a2b17df837c135028c8f
Dave Neary <dneary@gnome.org>
2018-10-18 13:18:01 +0000

    Add description of coding challenge in comment

--- src/main/java/org/eclipse/che/examples/FizzBuzz.java
+++ src/main/java/org/eclipse/che/examples/FizzBuzz.java
@@ -1,3 +1,17 @@
+/*
+ * EclipseCon Europe Che Challenge
+ *
+ * This program prints the numbers from 1 to 100, with every multiple of 3
+ * replaced by "Fizz", and every multiple of 5 replaced by "Buzz". Numbers
+ * divisible by both are replaced by "FizzBuzz". Otherwise, the program
+ * prints the number.
+ *
+ * Your mission, if you choose to accept it, is to change this program so that,
+ * in addition to the above, it prints "Fizz" for any number which includes a 3,
+ * and prints "Buzz" for any number including a 5. After modification, for
+ * example, the number 53 should be replaced by "FizzBuzz".
+ */
+
 package org.eclipse.che.examples;
 
 public class FizzBuzz {
